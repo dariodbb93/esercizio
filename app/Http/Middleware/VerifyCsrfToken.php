ebe3074203c5a631fe84f2677ef3202e521c2014
Dario Del Bel Belluz <dario@dario-dbb.it>
2022-11-05 08:40:33 +0100

    asd

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -11,7 +11,7 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array<int, string>
      */
-    protected $except = [ 'storeAdmin', 'storeTeam', 'storeDev', 'storeProjects'
+    protected $except = [ 'storeAdmin', 'storeTeam', 'storeDev', 'storeProjects', 'edit' , 'update' , 'destroy' , 'editP' , 'destroyP'
         //
     ];
 }

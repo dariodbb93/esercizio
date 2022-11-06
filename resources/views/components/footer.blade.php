d755828f7913b545b07d06ae7a7355af059de6af
unknown <dario@dario-dbb.it>
2022-11-06 16:02:58 +0100

    asd

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -6,7 +6,6 @@
             <!-- Facebook -->
             <p class="fw-bold" id ="footerText"> Powered by Dario Del Bel Belluz - Tutti i diritti riservati </p>
             <a id="icon" href="https://www.linkedin.com/in/dario-del-bel-belluz-82364a205/">
-               <p> contact me: dario@dario-dbb.it </p>
         </section>
     </div>
 </footer>
